5ce4c34712fd7187b2f6f65d52ac3207890e9318
xivilay <xivilay@gmail.com>
2021-11-13 13:42:10 +0100

    changed name in script

--- scripts/win/post_build.bat
+++ scripts/win/post_build.bat
@@ -2,7 +2,7 @@
 
 set OUT_DIR="%~dp0/../../dist"
 set BUILD_DIR="%~dp0/../../build"
-set PLUGIN_NAME="MidiScaleRemapper"
+set PLUGIN_NAME="ScaleRemapper"
 set PLUGIN_DIR="%BUILD_DIR%/%PLUGIN_NAME%_artefacts"
 
 rmdir %OUT_DIR% /s/q
