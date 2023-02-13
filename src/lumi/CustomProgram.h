6e52e68a44c2192698a519a6db1286baac60821a
xivilay <xivilay@gmail.com>
2023-02-13 19:42:59 +0100

    feat: full lumi colors support in 4 modes

--- src/lumi/CustomProgram.h
+++ src/lumi/CustomProgram.h
@@ -11,12 +11,15 @@ class CustomProgram : public roli::Block::Program {
         const int globalKeyColor = 34;
         const int rootKeyColor = 35;
         const int octaveId = 4;
+        int dimKeyColor;
         int keyColor;
         int rootColor;
         int root;
         int state;
         int colorMode;
         int fullColors[12];
+        int whiteKeys[14];
+        int remapIsOn;
     
         void initialise() {
             setUseDefaultKeyHandler(true, false);
@@ -24,6 +27,16 @@ class CustomProgram : public roli::Block::Program {
             rootColor = getLocalConfig(rootKeyColor);
             root = getLocalConfig(rootId);
             initColors();
+            initWhiteKeys();
+        }
+        void initWhiteKeys() {
+            int whiteKeyIndex = 0;
+            for (int i = 0; i < 24; i++) {
+                if (isWhiteKey(i)) {
+                    whiteKeys[whiteKeyIndex] = i;
+                    whiteKeyIndex++;
+                }
+            }
         }
         void initColors() {
             fullColors[0] = 0xcf3550;
@@ -38,27 +51,71 @@ class CustomProgram : public roli::Block::Program {
             fullColors[9] = 0x4d6db5;
             fullColors[10] = 0x564a9d;
             fullColors[11] = 0x8c55a2;
+            dimKeyColor = makeARGB(255, 0, 0, 0);
+        }
+        bool isWhiteKey(int noteIndex) {
+            int i = noteIndex % 12;
+            if (i == 1 || i == 3 || i == 6 || i == 8 || i == 10) return false;
+            return true;
+        }
+        int getLastBits (int k, int n) {
+            return k & ((1 << n) - 1);
+        }
+        int getMidBits (int k, int m, int n) {
+            return getLastBits(k >> m, n);
         }
         void renderKeys() {
-            for (int i = 0; i < 12; i++) {
-                int highlighted = (state >> i) & 1;
-                int num = 12 - 1 - i;
-                int color;
-                if (colorMode == 1) {
-                    color = fullColors[num];
-                } else {
-                    color = num == root ? rootColor : keyColor;
+            if (remapIsOn == 0) {
+                for (int i = 0; i < 12; i++) {
+                    int highlighted = (state >> i) & 1;
+                    int num = 12 - 1 - i;
+                    int color;
+                    if (colorMode == 1) {
+                        color = fullColors[num];
+                    } else {
+                        color = num == root ? rootColor : keyColor;
+                    }
+                    int fill = highlighted == 1 ? color : dimKeyColor;
+                    fillPixel(fill, num, 0);
+                    fillPixel(fill, num + 12, 0);
                 }
-                int fill = highlighted == 1 ? color : makeARGB(255, 0, 0, 0);
-                fillPixel(fill, num, 0);
-                fillPixel(fill, num + 12, 0);
+            } else {
+                int selectedCount = 0;
+                for (int i = 0; i < 12; i++) {
+                    selectedCount += ((state >> i) & 1);
+                }
+                int scaleIndex = 0;
+                for (int i = root; i < 12 + root; i++) {
+                    int highlighted = (state >> (11 - i%12)) & 1;
+                    int num = (12+i) % 12;
+                    
+                    if (highlighted == 1) {
+                        int w = scaleIndex;
+                        int color;
+                        while (w <= 13) {
+                            if (colorMode == 1) {
+                                color = fullColors[num];
+                            } else {
+                                color = num == root ? rootColor : keyColor;
+                            }
+                            fillPixel(color, whiteKeys[w], 0);
+                            w += selectedCount;
+                        }
+                        scaleIndex++;
+                    }
+                    if (!isWhiteKey(i-root)) {
+                        fillPixel(dimKeyColor, i-root, 0);
+                        fillPixel(dimKeyColor, i-root + 12, 0);
+                    }
+                }
             }
         }
         void handleMessage(int a, int b, int c) {
             if (state == a) return;
-            state = a;
-            root = a >> 12;
-            colorMode = a >> 16;
+            state =      getMidBits(a, 0, 12);
+            root =       getMidBits(a, 12, 4);
+            colorMode =  getMidBits(a, 12+4, 1);
+            remapIsOn =  getMidBits(a, 12+4+1, 1);
             int octave = getLocalConfig(octaveId);
             renderKeys();
 
