f98e8d085db0691c243e70ae77e6b735a1a226df
xivilay <xivilay@gmail.com>
2023-02-16 19:09:16 +0100

    chore: rename

--- scripts/win/post_build_android.bat
+++ scripts/win/post_build_android.bat
@@ -1,8 +1,9 @@
 @echo off
 
 set RELATIVE_DIR=..\..
-set OUT_DIR=%RELATIVE_DIR%\out
-set BUILD_DIR=RELATIVE_DIR%\android\app\build\outputs\apk\release
+set OUT_DIR=%RELATIVE_DIR%\dist
+set BUILD_DIR=%RELATIVE_DIR%\android\app\build\outputs\apk\release
 set ARTIFACT=app-release.apk
+set FILE_NAME=scale-remapper.apk
 
-robocopy %BUILD_DIR% %OUT_DIR% %ARTIFACT% /NFL /NDL /NJH /NJS /nc /ns /np
+robocopy %BUILD_DIR% %OUT_DIR% %ARTIFACT% /NFL /NDL /NJH /NJS /nc /ns /np & cd %OUT_DIR% & rename %ARTIFACT% %FILE_NAME%
